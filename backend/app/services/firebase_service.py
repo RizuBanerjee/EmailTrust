581eb62ed46181da5df825b7eb4df2ab662f9bab
Rizu Banerjee <rizubanerjee123@gmail.com>
2026-07-09 17:48:55 +0530

    Configure Firebase for Render deployment

--- backend/app/services/firebase_service.py
+++ backend/app/services/firebase_service.py
@@ -1,3 +1,5 @@
+import os
+
 import firebase_admin
 
 from firebase_admin import (
@@ -5,23 +7,66 @@ from firebase_admin import (
     auth
 )
 
-SERVICE_ACCOUNT_FILE = "serviceAccountKey.json"
 
-if not firebase_admin._apps:
+def initialize_firebase():
 
-    cred = credentials.Certificate(
-        SERVICE_ACCOUNT_FILE
+    if firebase_admin._apps:
+        return
+
+
+    firebase_config = {
+
+        "type":
+            "service_account",
+
+        "project_id":
+            os.getenv(
+                "FIREBASE_PROJECT_ID"
+            ),
+
+        "private_key":
+            os.getenv(
+                "FIREBASE_PRIVATE_KEY"
+            ).replace(
+                "\\n",
+                "\n"
+            ),
+
+        "client_email":
+            os.getenv(
+                "FIREBASE_CLIENT_EMAIL"
+            ),
+
+        "token_uri":
+            "https://oauth2.googleapis.com/token"
+
+    }
+
+
+    cred = (
+        credentials.Certificate(
+            firebase_config
+        )
     )
+
 
     firebase_admin.initialize_app(
         cred
     )
 
 
-def verify_firebase_token(
+initialize_firebase()
+
+
+
+def verify_token(
     token: str
 ):
 
-    return auth.verify_id_token(
-        token
+    decoded_token = (
+        auth.verify_id_token(
+            token
+        )
     )
+
+    return decoded_token
